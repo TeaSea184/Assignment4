4eeac9cec8b784e69798e47e39a4e1c5bc05e4fc
TeaSea184 <90568665+TeaSea184@users.noreply.github.com>
2021-09-13 13:39:30 +0200

    added test for TariffTable toString() method

--- src/test/java/TariffTest.java
+++ src/test/java/TariffTest.java
@@ -8,6 +8,14 @@ public class TariffTest {
         final Money calcTariff = CarParkSim.tariffs.getTariff(twoHours);
 
         Assertions.assertEquals(expectedTariff,calcTariff);
+    }
+    @Test
+    public void testTariffToString(){
+        final TariffTable testTariff = new TariffTable(1);
+        testTariff.addTariff(new TimePeriod(new Duration("day",1),new Duration("day",4)),
+                new Money("R150",CarParkSim.currency));
+        final String output = testTariff.toString();
 
+        Assertions.assertEquals("[1 day .. 4 days] : R150.00",output);
     }
 }
