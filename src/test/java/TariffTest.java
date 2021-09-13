fcb7c655f7bc5ede67c35b998eca75393be63c76
TeaSea184 <90568665+TeaSea184@users.noreply.github.com>
2021-09-13 12:57:51 +0200

    implementing first test case

--- src/test/java/TariffTest.java
+++ src/test/java/TariffTest.java
@@ -1,8 +1,13 @@
 import org.junit.jupiter.api.Test;
-
+import org.junit.jupiter.api.Assertions;
 public class TariffTest {
     @Test
-    public void testTariffOutput(){
+    public void testTariffCalc(){
+        Duration twoHours = new Duration("hour",2);
+        final Money expectedTariff = new Money("R30.00", CarParkSim.currency);
+        final Money calcTariff = CarParkSim.tariffs.getTariff(twoHours);
+
+        Assertions.assertEquals(expectedTariff,calcTariff);
 
     }
 }
